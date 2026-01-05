2522ebc9478cc3f7efa89ab1e071d931823ba79f
Hari Shankar Singh <hari@lmg8.tech>
2026-01-05 09:08:46 +0530

    Added some comment

--- SquareRoot/Program.cs
+++ SquareRoot/Program.cs
@@ -12,28 +12,29 @@
         {
             return n;
         }
-        int left = 1;
-        int right = n/2;
-        int result = 0;
+        int left = 1; //square root cannot be less than 1 (for n ≥ 2)
+        int right = n; //square root of any number > 1 is never greater than n/2
+        int result = 0; //stores best possible answer so far
         while (left <= right)
         {
             int mid=left+(right-left)/2;
-            long sqr=(long)mid*mid;
-            if (sqr == n)
+            long square = (long)mid * mid;
+            if (square== n)
             {
                 return mid;
             }
-            else if (sqr < n)
+            else if(square<n)
             {
                 result = mid;
-                left = mid + 1;
+                left = mid+1;
+                
             }
             else
             {
-                right = mid - 1;
+                right = right - 1;
             }
-
         }
-        return result; 
+
+        return result;
     }
 }
