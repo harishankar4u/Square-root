d53a9290d8f6bc3a0c4e43636fe4ec58fd295e96
Hari Shankar Singh <hari@lmg8.tech>
2026-01-05 08:29:53 +0530

    Add project files.

---
+++ SquareRoot/Program.cs
@@ -0,0 +1,39 @@
+﻿class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(sqroot(8));  //output 2
+        Console.WriteLine(sqroot(4));  //output 2
+        Console.WriteLine(sqroot(9));  //output 3
+    }
+    public static int sqroot(int n)
+    {
+        if(n<2)
+        {
+            return n;
+        }
+        int left = 1;
+        int right = n/2;
+        int result = 0;
+        while (left <= right)
+        {
+            int mid=left+(right-left)/2;
+            long sqr=(long)mid*mid;
+            if (sqr == n)
+            {
+                return mid;
+            }
+            else if (sqr < n)
+            {
+                result = mid;
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+
+        }
+        return result; 
+    }
+}
